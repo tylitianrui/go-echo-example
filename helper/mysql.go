23e1e93957280b4b9c2656439adfed1524c669ef
tyltr <tylitianrui@126.com>
2019-11-18 18:41:42 +0800

    单例mysql实例

---
+++ helper/mysql.go
@@ -0,0 +1,37 @@
+package helper
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/gommon/log"
+	"go-echo-example/runtime/setting"
+	"sync"
+)
+
+var (
+	err  error
+	once sync.Once
+	DB   *gorm.DB
+)
+
+func newDB() {
+	db := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8",
+		setting.G_DBConf.User,
+		setting.G_DBConf.Password,
+		setting.G_DBConf.Host,
+		setting.G_DBConf.Port,
+		setting.G_DBConf.DataBase,
+	)
+	fmt.Println(db)
+
+	if DB, err = gorm.Open(setting.G_DBConf.Type, db); err != nil {
+		log.Fatal("err:", err)
+		return
+	}
+
+}
+func GetDBInstance() *gorm.DB {
+	once.Do(newDB)
+	return DB
+}
---
+++ helper/mysql_test.go
@@ -0,0 +1,28 @@
+package helper
+
+import (
+	"fmt"
+	"go-echo-example/models"
+	"go-echo-example/runtime/setting"
+
+	//"log"
+	"testing"
+)
+
+func TestGetDBInstance(t *testing.T) {
+	setting.SetupConf("/Users/tyltr/GoSpace/go-echo-example/conf/app.ini")
+	db := GetDBInstance()
+	if db == nil {
+		t.Fail()
+	}
+	if !db.HasTable("account") {
+		t.Fail()
+	}
+	if !db.HasTable(&models.Account{}) {
+		t.Fail()
+	}
+	db1 := GetDBInstance()
+	fmt.Printf("%p\n", db1)
+	fmt.Printf("%p\n", db)
+
+}
