1dd6af6e4bd61ce3794a4946c16683af629248dd
tyltr <tylitianrui@126.com>
2019-11-19 22:49:31 +0800

    自定义server，初始化路由

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/labstack/echo"
+	"go-echo-example/routers"
 	"go-echo-example/runtime/setting"
+	"net/http"
 	"os"
+	"time"
 )
 
 var (
@@ -23,5 +27,20 @@ func main() {
 	confDir := flag.String("c", defaultConf, "")
 	flag.Parse()
 	setting.SetupConf(*confDir)
+	e := echo.New()
+	// 自定义
+	s := http.Server{
+		Addr:              ":9090",
+		ReadTimeout:       600 * time.Millisecond,
+		ReadHeaderTimeout: 60 * time.Millisecond,
+		WriteTimeout:      600 * time.Millisecond,
+		IdleTimeout:       600 * time.Millisecond,
+		MaxHeaderBytes:    1 << 20,
+	}
+
+	// 初始化路由
+	routers.InitRouters(e)
+
+	e.Logger.Fatal(e.StartServer(&s))
 
 }
